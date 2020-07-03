1384cb96ec9ea5def475ef076f8466d84cd8a3bb
Jaishir Bayuelo Crespo <jbayuelo@santaclaratech.es>
2020-07-02 20:31:29 -0500

    last modifications

--- app/backend/apps/tps/models.py
+++ app/backend/apps/tps/models.py
@@ -57,8 +57,9 @@ class Tienda(ModelBase):
     nombre = models.CharField(max_length=45)
     indicativo = models.PositiveIntegerField()
     whatsapp = models.PositiveIntegerField()
-    ubicacion = models.CharField(max_length=45)
+    ubicacion = models.CharField(max_length=45,blank=True, null=True)
     elementos = models.ManyToManyField('tps.Elemento', blank=True, related_name='+')
+    descripcion = models.TextField(blank=True, null=True)
 
     def __str__(self):
         return str(self.id)
